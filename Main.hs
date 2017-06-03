c8460b404be430f147b5f58f00d2272e17654b80
loovjo <jonathan.loov@gmail.com>
2017-06-03 13:52:36 +0200

    Added debug

--- Main.hs
+++ Main.hs
@@ -2,23 +2,28 @@ import Base
 import Parser
 import ActionTree
 import Generator
+import System.Environment
 
 import System.IO
 import Data.List
 import Data.Foldable
 
-main = do
+main = run =<< getArgs
+
+run args = do
+    let debug x = if elem "-d" args then hPutStrLn stderr x else return ()
+
     codeInf <- hGetContents stdin
     let code = foldr' (:) [] codeInf
         tokens = tokenize code
-    hPutStrLn stderr $ show tokens
+    debug $ "Tokens:   " ++ intercalate " " tokens
     let ast_ = makeAst tokens
         at_ = pr2Map makeAT ast_
         code_ = prMap makeCode at_
     case ( ast_, at_, code_ ) of
         (Ok ast, Ok at, Ok code) ->
             do 
-                hPutStrLn stderr $ "Ast:\n" ++ ( intercalate "\n" $ prettify ast )
-                hPutStrLn stderr $ "AT: " ++ show at
+                debug $ "Ast:\n" ++ ( intercalate "\n" $ prettify ast )
+                debug $ "AT: " ++ show at
                 putStrLn $ intercalate "\n" code
         (_, _, Err err) -> hPutStrLn stderr $ "Code couldn't compile! Error: " ++ err
